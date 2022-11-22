7b01545bded67a461cefbd15008abe006a379d92
alexdneprin <Ruvoni45>
2022-11-22 12:26:29 +0000

    Added project

---
+++ AC Group 2: Git/AC Group 2: Git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AC Group 2: Git
+//
+//  Created by Oleksandr Slobodianiuk on 22.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
